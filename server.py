108d8d595a928d58038b3cfcccd13ee38cea0996
cameronbolton <39877913+cameronbolton@users.noreply.github.com>
2025-11-29 14:34:17 -0600

    game list path handling

--- server.py
+++ server.py
@@ -4,13 +4,18 @@ import socketserver
 
 from http import HTTPStatus
 
-
 class Handler(http.server.SimpleHTTPRequestHandler):
+    def get_game_list(self)
+        msg = f"games list: {self.games_list}"
+        self.wfile.write(msg.encode())
+
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         msg = 'Hello! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
+        if self.path == "/game_list":
+            self.get_game_list()
 
 
 port = int(os.getenv('PORT', 80))
